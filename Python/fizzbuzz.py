7943fa179a3492726793ba9ddfd7d1cffe073f32
Chris Sundström <chris.ramboo@gmail.com>
2023-10-19 21:47:55 +0200

    Added fizzbuzz.py

---
+++ Python/fizzbuzz.py
@@ -0,0 +1,27 @@
+def main():
+    fizzbuzz()
+
+
+def fizzbuzz():
+    """
+    Description:
+        Loop through all numbers 1 <=> 100. 
+        If multiple of 3: Print "Fizz"
+        If multiple of 5: Print "Buzz"
+        If multiple of 15: Print "FizzBuzz"
+        Else: Prints the number.
+        Function takes no argument and has no return value.
+    """
+    for i in range(1, 101):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        
+
+if __name__ == "__main__":
+    main()
