96e1e06dc4ae176f7e9923089c09c71b3c14a291
Chris Sundström <chris.ramboo@gmail.com>
2023-10-19 23:07:52 +0200

    Minor changes to docstring and added comments.

--- Python/binary_search.py
+++ Python/binary_search.py
@@ -2,13 +2,17 @@ def binary_search(element_to_find, list_to_search):
     """
     Description:
         Perform a binary search to find if the target element is in the list.
+        Time complexity: O(log n)
 
     Arguments:
         element_to_find (any): The element to search for.
         list_to_search (list): The list in which to perform the search.
 
     Returns:
-        bool: True if the target element is found, False otherwise.
+        bool: True if the target element is found. False otherwise.
+
+    IMPORTANT:
+        Please observe that the list MUST be sorted in acending order.
     """
     assert isinstance(list_to_search, list), "'list_to_search' must a list."
 
@@ -30,8 +34,8 @@ def binary_search(element_to_find, list_to_search):
         if list_to_search[middle] == element_to_find:
             return True
         elif list_to_search[middle] > element_to_find:
-            high = middle - 1
+            high = middle - 1   # Search left half.
         else:
-            low = middle + 1
+            low = middle + 1    # Search right half.
 
     return False
