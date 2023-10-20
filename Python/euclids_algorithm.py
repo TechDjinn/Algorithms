36bc71e1b9800772cd8ae3853c12f2f4e96a7021
Chris Sundström <chris.ramboo@gmail.com>
2023-10-21 00:12:15 +0200

    added euclids_algorithm.py

---
+++ Python/euclids_algorithm.py
@@ -0,0 +1,28 @@
+def gcd(a, b):
+    """
+    Description:
+        Finds the greatest common denominator of two positive integers.
+
+    Arguments:
+        a (int): one of the numbers.
+        b (int): the other the numbers.
+
+    Returns:
+        int: the greatest common denominator
+
+    Explaination of principle:
+        The Euclidean algorithm is based on the principle 
+        that the greatest common divisor of two numbers 
+        does not change if the larger number is replaced by 
+        its difference with the smaller number.
+    """
+    assert isinstance(a, int) and a > 0, "'a' must be a positive int"
+    assert isinstance(b, int) and b > 0, "'b' must be a positive int"
+    
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a
+
