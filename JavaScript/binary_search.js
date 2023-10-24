62837429655f681f2878374a039a2d48c18c9d00
Chris Sundström <chris.ramboo@gmail.com>
2023-10-24 22:23:20 +0200

    Function now return boolean. Correct argument name.

--- JavaScript/binary_search.js
+++ JavaScript/binary_search.js
@@ -8,30 +8,30 @@ function main(){
 
     let n = 50;
 
-    if (binary_search(n, arr) === n){
+    if (binary_search(n, arr) === true){
         console.log("Element found!");
     } else{
         console.log("Element NOT found!");
     }
 }
 
-function binary_search(element_to_fint, array_to_search){
+function binary_search(element_to_find, array_to_search){
     let low = 0;
     let high = array_to_search.length;
 
     while (low <= high){
         let mid = Math.floor((low + high) / 2);
 
-        if (array_to_search[mid] === element_to_fint){
-            return element_to_fint;
-        } else if (array_to_search[mid] > element_to_fint){
+        if (array_to_search[mid] === element_to_find){
+            return true;
+        } else if (array_to_search[mid] > element_to_find){
             high = mid - 1;
         } else{
             low = mid + 1;
         }
     }
 
-    return -1;
+    return false;
 }
 
 main();
