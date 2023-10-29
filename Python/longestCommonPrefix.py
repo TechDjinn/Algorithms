af0c511865a5c936c50a18ad34a9db73711f3004
Chris Sundström <chris.ramboo@gmail.com>
2023-10-29 15:52:56 +0100

    Added longestComminPrefix.py

---
+++ Python/longestCommonPrefix.py
@@ -0,0 +1,16 @@
+def longestCommonPrefix(strs):      
+    # Declaration of variable
+    i = 0
+
+    # Check for longest common prefix
+    for i in range(1, len(strs[0]) + 1):
+        # Compare each string agains the prefix of string at index 0.
+        for word in strs:
+            if word[:i] != strs[0][:i]:
+                # Not the same.
+                return strs[0][:i-1]
+                
+    # All strings are equal.
+    return strs[0][:i]
+
+print(longestCommonPrefix(["abcde", "abcf", "abkli"]))
